b84571ef5bc4a07be2bb76379cae7db08659f7bd
huheee <huheee2002@naver.com>
2018-01-08 15:39:10 +0900

    Add files via upload

---
+++ Assets/sample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sample : MonoBehaviour {
+
+
+	int indexNumber = 10;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(indexNumber);
+	}
+	
+	void OnMouseDown(){
+			Debug.Log("You clicked me");
+		}
+
+		void OnMouseUp(){
+			Debug.Log("Mouse Up");
+		}
+
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
